a049c6c592cd20b44b426cbc08aa75b44742c6b8
saniyaj mallik <saniyajmallik12@gmail.com>
2024-02-11 11:27:37 +0530

    custom button

--- src/App.jsx
+++ src/App.jsx
@@ -2,30 +2,45 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-import { GoogleLogin } from "@react-oauth/google";
+import { GoogleLogin, useGoogleLogin } from "@react-oauth/google";
 import { jwtDecode } from "jwt-decode";
 
 function App() {
-  const [user, setuser] = useState({})
-  
+	const [user, setuser] = useState({});
+
+  const login = useGoogleLogin({
+    onSuccess: tokenResponse => {
+      let decoded = jwtDecode(tokenResponse.credential);
+      setuser(decoded);
+      console.log("decode",decoded);
+      console.log("tokenResponse",tokenResponse);
+    }
+
+  });
+
+
 	return (
 		<>
 			<GoogleLogin
 				onSuccess={(credentialResponse) => {
-          let decoded = jwtDecode(credentialResponse.credential)
-          setuser(decoded)
+					let decoded = jwtDecode(credentialResponse.credential);
+					setuser(decoded);
 					console.log(decoded);
-          console.log(user);
+					console.log(user);
 				}}
 				onError={() => {
 					console.log("Login Failed");
 				}}
 			/>
 
-      <p>{user?.email}</p>
-      <p>{user?.given_name}</p>
-      <p>{user?.family_name}</p>
-      <img src={user?.picture} alt="" />
+      <button onClick={() => login()}>Login with Google</button>
+
+			<p>{user?.email}</p>
+
+			<p>
+				{user?.given_name} {user?.family_name}
+			</p>
+			<img src={user?.picture} alt='' />
 		</>
 	);
 }
