ef75ac6395d9b00ab2bfa4f1892d4ada2f9c4b3e
saniyaj mallik <saniyajmallik12@gmail.com>
2024-02-11 11:20:31 +0530

    user

--- src/App.jsx
+++ src/App.jsx
@@ -6,13 +6,16 @@ import { GoogleLogin } from "@react-oauth/google";
 import { jwtDecode } from "jwt-decode";
 
 function App() {
+  const [user, setuser] = useState({})
   
 	return (
 		<>
 			<GoogleLogin
 				onSuccess={(credentialResponse) => {
           let decoded = jwtDecode(credentialResponse.credential)
+          setuser(decoded)
 					console.log(decoded);
+          console.log(user);
 				}}
 				onError={() => {
 					console.log("Login Failed");
